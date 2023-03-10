20dd7e147ef10cdefe4736b30b87c5ff8e3a34ec
Dola1122 <dolagamer2002@gmail.com>
2023-03-10 14:48:31 +0200

    paul code is pulled

--- backend/views.py
+++ backend/views.py
@@ -39,48 +39,48 @@ class LogoUploadView(APIView):
     # serializer = LogoSerializer(data=request.data)
     # return Response({'serializer': serializer})
 
-  def post(self, request, *args, **kwargs):
-    cd = ColorDescriptor((8, 12, 3))
-    # Get logo data from the api post request 
-    data = request.data
-    logo = data['image']
-    # initialize gridFs to store logo in database 
-    fs = gridfs.GridFS(database)
-    logoId = fs.put(logo, name=logo.name, flag='uploaded')
-    # process numpy array with logodata stored in database
-    nparr = np.frombuffer(fs.get(logoId).read(), np.uint8)
-    # decode numpy array and descibe image for comparison
-    image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-    features = cd.describe(image)
-    # query for logo indexes from the database then store in an array to search for matching logos
-    logo_index = index_database.find({}, {'features': 1, 'file_id': 1})
-    index_list = []
-    for csv_index in logo_index:
-       csv_index['features'].insert(0, csv_index['file_id'])
-       index_list.append(csv_index['features'])
-    searcher = Searcher(index_list)
-    results = searcher.search(features)
-    features = [str(f) for f in features]
-    
-    # query to check if the uploaded logo index is already in the database
-    # if not in database store the index 
-    # if already in database delete the uploaded logo 
-    
-    check_index = index_database.find_one({'features': features}, {'features': 1})
-    if check_index is None:
-        index_database.insert_one({'file_id':logoId, 'features':features})
-    else:
-       fs.delete(logoId)
-    limit = 3
-    # create an array of matching logo urls to be called in a get api to display logos in frontend.
-    api_result = []
-    results = results[:limit]
-    for result in results:
-      url = 'http://127.0.0.1:8000/api/image/'
-      logo = database.fs.files.find_one({'_id':result[1]})
-      arr =  url+str(logo['_id'])
-      api_result.append(arr)
-    return JsonResponse({'results': api_result}, status=status.HTTP_201_CREATED)
+    def post(self, request, *args, **kwargs):
+        cd = ColorDescriptor((8, 12, 3))
+        # Get logo data from the api post request 
+        data = request.data
+        logo = data['image']
+        # initialize gridFs to store logo in database 
+        fs = gridfs.GridFS(database)
+        logoId = fs.put(logo, name=logo.name, flag='uploaded')
+        # process numpy array with logodata stored in database
+        nparr = np.frombuffer(fs.get(logoId).read(), np.uint8)
+        # decode numpy array and descibe image for comparison
+        image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+        features = cd.describe(image)
+        # query for logo indexes from the database then store in an array to search for matching logos
+        logo_index = index_database.find({}, {'features': 1, 'file_id': 1})
+        index_list = []
+        for csv_index in logo_index:
+            csv_index['features'].insert(0, csv_index['file_id'])
+            index_list.append(csv_index['features'])
+        searcher = Searcher(index_list)
+        results = searcher.search(features)
+        features = [str(f) for f in features]
+
+        # query to check if the uploaded logo index is already in the database
+        # if not in database store the index 
+        # if already in database delete the uploaded logo 
+
+        check_index = index_database.find_one({'features': features}, {'features': 1})
+        if check_index is None:
+            index_database.insert_one({'file_id':logoId, 'features':features})
+        else:
+            fs.delete(logoId)
+        limit = 3
+        # create an array of matching logo urls to be called in a get api to display logos in frontend.
+        api_result = []
+        results = results[:limit]
+        for result in results:
+            url = 'http://127.0.0.1:8000/api/image/'
+            logo = database.fs.files.find_one({'_id':result[1]})
+            arr =  url+str(logo['_id'])
+            api_result.append(arr)
+        return JsonResponse({'results': api_result}, status=status.HTTP_201_CREATED)
 
 
 class ImageAPIView(APIView):
